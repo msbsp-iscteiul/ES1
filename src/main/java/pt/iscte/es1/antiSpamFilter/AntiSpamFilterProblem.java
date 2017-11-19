ed56f7b16aee62720439f1c73c300532bb589765
Miguel Amaro <macao1@iscte-iul.pt>
2017-11-19 19:28:48 +0000

    Documenting AntiSpamFilter

--- src/main/java/pt/iscte/es1/antiSpamFilter/AntiSpamFilterProblem.java
+++ src/main/java/pt/iscte/es1/antiSpamFilter/AntiSpamFilterProblem.java
@@ -9,13 +9,17 @@ import org.uma.jmetal.solution.DoubleSolution;
 import pt.iscte.es1.antiSpamFilter.domain.Message;
 import pt.iscte.es1.antiSpamFilter.domain.WeightedRule;
 
-/**
- * 
- *
- */
+
 @SuppressWarnings("serial")
 public class AntiSpamFilterProblem extends AbstractDoubleProblem {
 
+	/**
+	 * The AntiSpamFilterProblem class is an extension of the JMetal AbstractDoubleProblem class and is used to evaluate a given set of
+	 * anti-spam rules for given lists of spam and ham messages. 
+	 * 
+	 *
+	 */
+	
 	private List<Message> spam;
 	private List<Message> ham;
 	private List<WeightedRule> rules;
@@ -23,10 +27,13 @@ public class AntiSpamFilterProblem extends AbstractDoubleProblem {
 	public final static int INDEX_FALSE_NEGATIVE = 0;
 	public final static int INDEX_FALSE_POSITIVE = 1;
 
+	
 	/**
-	 * @param rules
-	 * @param ham
-	 * @param spam
+	 * Default Constructor that accepts a list of Rules, a list of HAM messages, a list of SPAM messages and sets the size of the rules list;
+	 * 
+	 * @param rules -> WeightedRule list that holds all Rules;
+	 * @param ham -> Message List that holds all HAM Messages;
+	 * @param spam -> Message List that holds all SPAM Messages;
 	 */
 	public AntiSpamFilterProblem(List<WeightedRule> rules, List<Message> ham, List<Message> spam) {
 
@@ -38,7 +45,9 @@ public class AntiSpamFilterProblem extends AbstractDoubleProblem {
 	}
 
 	/**
-	 * @param numberOfVariables
+	 * Constructor that accepts an Integer NumberOfVariables and sets the lower and upper bounds for each variable on the LowerLimit and UpperLimit Lists;
+	 * 
+	 * @param numberOfVariables -> Integer value that defines the number of variables;
 	 */
 	public AntiSpamFilterProblem(Integer numberOfVariables) {
 		setNumberOfVariables(numberOfVariables);
@@ -57,9 +66,27 @@ public class AntiSpamFilterProblem extends AbstractDoubleProblem {
 		setUpperLimit(upperLimit);
 	}
 
+
+	/**
+	 * Method used to evaluate a given solution for the current Anti-SPAM problem.
+	 * 
+	 * It accepts a DoubleSolution interface that extends the JMETAL Solution class and sets the counters for FalsePositives and 
+	 * FalseNegatives to zero.
+	 * 
+	 * Then for each message on the spam and ham lists, its analyzed if the current message has the same words as the Rules list and if so, 
+	 * the double aux variable is incremented with the weight of that rule.
+	 * 
+	 * Finally, the total weight on the double aux variable is compared with the Threshold and the correspondent FalseNegative or
+	 * FalsePositive counter is incremented;
+	 * 
+	 * 
+	 * @param solution -> DoubleSolution interface that extends JMetals class Solution;
+	 * /
+	
 	/* (non-Javadoc)
 	 * @see org.uma.jmetal.problem.Problem#evaluate(java.lang.Object)
 	 */
+
 	@Override
 	public void evaluate(DoubleSolution solution) {
 		int falseNegative = 0;
