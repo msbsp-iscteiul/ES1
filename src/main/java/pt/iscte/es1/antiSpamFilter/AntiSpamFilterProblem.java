749811fb84ddc42b0fb2834dfbfa59413c75a11b
msbsp-iscteiul <Mauro_Pinto@iscte.pt>
2017-11-19 20:40:10 +0000

    formats AntiSpamFilterProblem comments

--- src/main/java/pt/iscte/es1/antiSpamFilter/AntiSpamFilterProblem.java
+++ src/main/java/pt/iscte/es1/antiSpamFilter/AntiSpamFilterProblem.java
@@ -10,16 +10,14 @@ import pt.iscte.es1.antiSpamFilter.domain.Message;
 import pt.iscte.es1.antiSpamFilter.domain.WeightedRule;
 
 
+/**
+ * The AntiSpamFilterProblem class is an extension of the JMetal
+ * AbstractDoubleProblem class and is used to evaluate a given set of anti-spam
+ * rules for given lists of spam and ham messages.
+ */
 @SuppressWarnings("serial")
 public class AntiSpamFilterProblem extends AbstractDoubleProblem {
 
-	/**
-	 * The AntiSpamFilterProblem class is an extension of the JMetal AbstractDoubleProblem class and is used to evaluate a given set of
-	 * anti-spam rules for given lists of spam and ham messages. 
-	 * 
-	 *
-	 */
-	
 	private List<Message> spam;
 	private List<Message> ham;
 	private List<WeightedRule> rules;
@@ -29,11 +27,15 @@ public class AntiSpamFilterProblem extends AbstractDoubleProblem {
 
 	
 	/**
-	 * Default Constructor that accepts a list of Rules, a list of HAM messages, a list of SPAM messages and sets the size of the rules list;
+	 * Default Constructor that accepts a list of Rules, a list of HAM messages,
+	 * a list of SPAM messages and sets the size of the rules list;
 	 * 
-	 * @param rules -> WeightedRule list that holds all Rules;
-	 * @param ham -> Message List that holds all HAM Messages;
-	 * @param spam -> Message List that holds all SPAM Messages;
+	 * @param rules
+	 *            WeightedRule list that holds all Rules;
+	 * @param ham
+	 *            Message List that holds all HAM Messages;
+	 * @param spam
+	 *            Message List that holds all SPAM Messages;
 	 */
 	public AntiSpamFilterProblem(List<WeightedRule> rules, List<Message> ham, List<Message> spam) {
 
@@ -45,9 +47,12 @@ public class AntiSpamFilterProblem extends AbstractDoubleProblem {
 	}
 
 	/**
-	 * Constructor that accepts an Integer NumberOfVariables and sets the lower and upper bounds for each variable on the LowerLimit and UpperLimit Lists;
+	 * Constructor that accepts an Integer NumberOfVariables and sets the lower
+	 * and upper bounds for each variable on the LowerLimit and UpperLimit
+	 * Lists;
 	 * 
-	 * @param numberOfVariables -> Integer value that defines the number of variables;
+	 * @param numberOfVariables
+	 *            Integer value that defines the number of variables;
 	 */
 	public AntiSpamFilterProblem(Integer numberOfVariables) {
 		setNumberOfVariables(numberOfVariables);
@@ -68,25 +73,26 @@ public class AntiSpamFilterProblem extends AbstractDoubleProblem {
 
 
 	/**
-	 * Method used to evaluate a given solution for the current Anti-SPAM problem.
+	 * Method used to evaluate a given solution for the current Anti-SPAM
+	 * problem.
 	 * 
-	 * It accepts a DoubleSolution interface that extends the JMETAL Solution class and sets the counters for FalsePositives and 
-	 * FalseNegatives to zero.
+	 * It accepts a DoubleSolution interface that extends the JMETAL Solution
+	 * class and sets the counters for FalsePositives and FalseNegatives to
+	 * zero.
 	 * 
-	 * Then for each message on the spam and ham lists, its analyzed if the current message has the same words as the Rules list and if so, 
-	 * the double aux variable is incremented with the weight of that rule.
+	 * Then for each message on the spam and ham lists, its analyzed if the
+	 * current message has the same words as the Rules list and if so, the
+	 * double aux variable is incremented with the weight of that rule.
 	 * 
-	 * Finally, the total weight on the double aux variable is compared with the Threshold and the correspondent FalseNegative or
-	 * FalsePositive counter is incremented;
+	 * Finally, the total weight on the double aux variable is compared with the
+	 * Threshold and the correspondent FalseNegative or FalsePositive counter is
+	 * incremented;
 	 * 
 	 * 
-	 * @param solution -> DoubleSolution interface that extends JMetals class Solution;
-	 * /
-	
-	/* (non-Javadoc)
+	 * @param solution
+	 *            DoubleSolution interface that extends JMetals class Solution;
 	 * @see org.uma.jmetal.problem.Problem#evaluate(java.lang.Object)
 	 */
-
 	@Override
 	public void evaluate(DoubleSolution solution) {
 		int falseNegative = 0;
